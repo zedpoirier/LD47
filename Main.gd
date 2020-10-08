409ced07688f5261cb3630d141233534d09b857d
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2020-10-08 17:59:45 -0400

    Added Score Tracker

--- Main.gd
+++ Main.gd
@@ -15,13 +15,21 @@ var v3s = 0.615
 var loopDestOffset = 0.75
 var loopTriggerOffset = 0.85
 
+var lastBestScore = 0
+var currentScore = 0
+var loopScoreOffset = 0
+var playerStartY = 0
+
 func _ready():
 	$Path/Follow.unit_offset = startOffset
 	$Player.setPosition($Path/Follow.global_position)
+	playerStartY = $Player.position.y
 	print($Player.transform.get_origin())
 
 func _input(event):
-	if !event.is_echo():
+	if !event.is_echo(): # is the input the same the last check?
+		if event is InputEventMIDI:
+			pass
 		if event is InputEventMouseButton:
 			if event.pressed:
 					boulderForce += sisyphorce
@@ -32,15 +40,19 @@ func _input(event):
 				elif !event.echo:
 					boulderForce += sisyphorce
 
-func _process(delta):
+func _process(_delta):
 	checkProgress()
+	scoreUpdate()
 	# setup boulderForce contraints
 	boulderForce = min(boulderForce, 3.0)
 	# apply boulderForce to offset and move player
 	$Path/Follow.offset += boulderForce
 	$Player.setPosition($Path/Follow.position)
 	$Player.setRotation(boulderForce)
-	# control particle dust effect
+	
+	dust(); # control particle dust effect
+
+func dust():
 	if boulderForce < 0:
 		$Dust.emitting = true
 		$Dust.transform = Transform2D($Player.transform)
@@ -48,13 +60,24 @@ func _process(delta):
 	else:
 		$Dust.emitting = false
 
+func scoreUpdate():
+	$Score.position = $Player.position
+	currentScore = (playerStartY - $Player.position.y) * 0.1 + loopScoreOffset
+	if currentScore > lastBestScore:
+		$Score/Control/Value.text = str(stepify(currentScore, 1)) + "m"
+		lastBestScore = currentScore
+	pass
+
 func checkProgress():
 	if $Path/Follow.unit_offset < 0.01:
 		$EndScreen/Control/WinText.visible = true
 		return
 	elif $Path/Follow.unit_offset > loopTriggerOffset:
+		var a = (playerStartY - $Player.position.y) * 0.1
 		$Path/Follow.unit_offset = loopDestOffset
 		$Player.setPosition($Path/Follow.position)
+		var b = (playerStartY - $Player.position.y) * 0.1
+		loopScoreOffset += a - b
 	elif $Path/Follow.unit_offset > v3s:
 		plateaud = false
 	elif $Path/Follow.unit_offset > v2e:
@@ -74,7 +97,3 @@ func _on_GravityTimer_timeout():
 	else:
 		if boulderForce - gravity > 0:
 			boulderForce -= gravity
-
-
-func _on_Timer_timeout():
-	get_tree().quit()
